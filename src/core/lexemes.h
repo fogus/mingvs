df33f14a75e2e1b9078733214bfcdaa0a618c132
fogus <mefogus@gmail.com>
2014-06-06 10:54:53 -0400

    Adding symbol table node struct

--- src/core/lexemes.h
+++ src/core/lexemes.h
@@ -30,9 +30,9 @@ struct symbol_hash_node_t
     struct symbol_hash_node_t *next;
     long                       count;                   /* The reference count */
     int                        depth;                   /* The symbol's call-depth */
-    unsigned int               permanent       : 1;
-    unsigned int               ephemeral       : 1;     /* Refers to potentially gc'd symbols */
-    unsigned int               needed          : 1;
+    boolean                    permanent       : 1;
+    boolean                    ephemeral       : 1;     /* Refers to potentially gc'd symbols */
+    boolean                    needed          : 1;
     unsigned int               bucket          : 29;    /* The index in the symbol table */
     char *                     contents;
     char *                     decl_part;
