4e5445b264806b3d411147096f3ada27a9252710
fogus <mefogus@gmail.com>
2014-06-06 09:41:07 -0400

    Some useful constants

---
+++ src/core/constants.h
@@ -0,0 +1,17 @@
+#ifndef __CONSTANT_H__
+#define __CONSTANT_H__
+
+#define FALSE         0
+#define TRUE          1
+#define OFF           FALSE
+#define ON            TRUE
+#define NEGATIVE      OFF
+#define POSITIVE      ON
+
+#define APP_NAME      "vander"
+#define PROMPT        "v> "
+
+#define VERSION       "0.0.0"
+
+#endif
+
