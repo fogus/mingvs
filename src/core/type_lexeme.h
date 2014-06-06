022d6148901cfdc36cd806fae0203371d65ecd25
fogus <mefogus@gmail.com>
2014-06-06 14:51:03 -0400

    C-style namespacing:p

---
+++ src/core/type_lexeme.h
@@ -0,0 +1,44 @@
+#ifndef __TYPE_LEXEMES_H__
+#define __TYPE_LEXEMES_H__
+
+#ifdef LOCALE
+#undef LOCALE
+#endif
+
+#ifdef __TYPE_LEXEMES_C__
+#define LOCALE
+#else
+#define LOCALE extern
+#endif
+
+#include "setup.h"
+#include "gc.h"
+
+#include <stdlib.h>
+
+#ifndef SYMBOL_HASH_SZ
+#define SYMBOL_HASH_SZ          63559L
+#endif
+
+/**
+ *  \struct symbol_hash_node_t
+ *
+ *  Holds the runtime information for individual symbols.
+ *
+ **/
+struct symbol_table_node_t
+{
+    struct symbol_table_node_t* next;
+    char *                      contents;
+    type_decl                   t;
+    struct gc_descriptor_t      gc_descr;
+};
+
+typedef struct symbol_table_node_t SYMBOL_T;
+
+/* Lexeme function signatures */
+
+LOCALE void init_symbol_table(env_ref, struct symbol_table_node_t **);
+LOCALE void* intern_symbol(env_ref, char *);
+
+#endif
---
+++ src/core/type_scalar.h
@@ -0,0 +1,32 @@
+#ifndef __TYPE_SCALARS_H__
+#define __TYPE_SCALARS_H__
+
+#ifdef LOCALE
+#undef LOCALE
+#endif
+
+#ifdef __TYPE_SCALARS_C__
+#define LOCALE
+#else
+#define LOCALE extern
+#endif
+
+#include <stdlib.h>
+
+#include "setup.h"
+#include "type_lexeme.h"
+
+struct rt_scalar_data_t
+{
+    void *              true_symbol;
+    void *              false_symbol;
+    void *              nil_symbol;
+    void *              nothing_symbol;
+    void *              notset_symbol;
+    void *              plus_inf;                 // Plus infinity
+    void *              neg_inf;                  // Negative infinity
+    void *              zero;
+    SYMBOL_T **         symbol_table;
+};
+
+#endif
