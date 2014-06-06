82c6d46ac5fe3cc8e69219b83032a91521ac994c
fogus <mefogus@gmail.com>
2014-06-06 15:25:01 -0400

    C-style namespacing:p

---
+++ src/core/core_env.h
@@ -0,0 +1,42 @@
+#ifndef __CORE_ENVIRONMENT_H__
+#define __CORE_ENVIRONMENT_H__
+
+#ifndef __TYPE_SCALARS_H__
+#include "type_scalar.h"
+#endif
+
+#ifdef LOCALE
+#undef LOCALE
+#endif
+
+#ifdef __CORE_ENVIRONMENT_C__
+#define LOCALE
+#else
+#define LOCALE extern
+#endif
+
+#include "kernel.h"
+
+#define USER_ENVIRONMENT_DATA_INDEX   70
+#define MAXIMUM_ENVIRONMENT_POSITIONS 100
+
+struct env_t
+{
+    unsigned int   initialized : 1;
+    unsigned long  index;
+    void*          context;
+    void**         data;
+    struct env_t*  next;
+    // TODO: Cleanup function?
+};
+
+typedef struct env_t  ENV_T;
+typedef struct env_t* ENV_PTR;
+
+#define get_env_data(env, position)        (((struct env_t *)env)->data[position])
+#define set_env_data(env, position, value) (((struct env_t *)env)->data[position] = value)
+
+LOCALE void env_alloc(env_ref, unsigned int, unsigned long);
+
+#endif
+
---
+++ src/core/kernel.h
@@ -0,0 +1,18 @@
+#ifndef __KERNEL_H__
+#define __KERNEL_H__
+
+#include "kernel_constants.h"
+
+typedef unsigned int boolean;
+enum { false, true };
+
+typedef char* type_decl;
+
+typedef void* env_ref;
+
+#define globle
+
+#define SYMBOL_DATA_INDEX 0b110001
+
+#endif
+
