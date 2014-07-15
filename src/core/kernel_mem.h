cc6813937fe159fa0d1e8416a00bee9bdf2bfb59
fogus <mefogus@gmail.com>
2014-07-15 14:44:46 -0400

    Placeholder for alloc/dealloc funs

--- src/core/kernel_mem.h
+++ src/core/kernel_mem.h
@@ -20,5 +20,18 @@ struct memory_pool_t
     struct memory_pool_t *next;
 };
 
+struct memory_data_t
+{
+    long int               amount;
+    long int               calls;
+    BOOL                   is_conserving;
+    struct memory_pool_t*  temp_ptr;
+    struct memory_pool_t** mem_table;
+    size_t                 temp_sz;
+    //  TODO: out of mem func 
+};
+
+LOCALE void init_memory(void *);
+
 #endif
 
