3840ffd320598113c578d19d7bb0d55cf3e53492
fogus <mefogus@gmail.com>
2014-07-28 15:20:15 -0400

    Add stub for sysdep env alloc

--- src/core/kernel_sysdep.h
+++ src/core/kernel_sysdep.h
@@ -1,3 +1,27 @@
+#ifndef __SYSDEP_H__
+#define __SYSDEP_H__
 
+#ifndef __TYPE_SCALAR_H__
+#include "type_scalar.h"
+#endif
+
+#ifndef _STDIO_INCLUDED_
+#define _STDIO_INCLUDED_
+#include <stdio.h>
+#endif
+
+#include <setjmp.h>
+
+#ifdef LOCALE
+#undef LOCALE
+#endif
+
+#ifdef __SYSDEP_C__
+#define LOCALE
+#else
+#define LOCALE extern
+#endif
 
 LOCALE void* env_create(env_ref, struct symbol_table_node_t**);
+
+#endif
